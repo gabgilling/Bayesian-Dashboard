0855dda956e38deaac317900ec66429f29b21545
Gabriel Gilling <gabrielgilling@ibm.com>
2021-05-27 15:47:08 -0400

    initial commits

---
+++ index.py
@@ -0,0 +1,11 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+
+from app import app
+
+import pages
+
+server = app.server
+
